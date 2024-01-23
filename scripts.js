d970747c6539a4405fda9f1f983d0bc7477d0f16
gpopowic <gmpopowich@gmail.com>
2024-01-23 13:50:09 -0800

    selected buttons and display

---
+++ scripts.js
@@ -0,0 +1,4 @@
+const display = document.querySelector(".display");
+const time = document.querySelector(".time");
+const start = document.querySelector(".start");
+const set = document.querySelector(".set");
